2dc9139d19a3c1df6cae1c8e523319691ecbef86
Navleen Kaur <navleenkaur@Navleens-MacBook-Pro.local>
2022-08-10 11:27:56 -0400

    Set up Game class

--- studio_game.rb
+++ studio_game.rb
@@ -25,6 +25,35 @@ class Player
 
 end
 
+class Game
+
+    def initialize(title)
+        @title = title
+        @players = []
+    end
+
+    attr_reader :title
+
+    def add_player(a_player)
+        @players.push(a_player)
+    end
+
+    def play
+        puts "There are #{@players.size} players in #{@title}:"
+        @players.each do |player|
+            puts player
+        end
+
+        @players.each do |player|
+            player.blam
+            player.w00t
+            player.w00t
+            puts player
+        end
+    end
+
+end
+
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
@@ -52,3 +81,15 @@ players.pop
 player4 = Player.new("Shemp", 90)
 players.push(player4)
 puts players
+
+
+puts "____________________"
+
+knuckleheads = Game.new("Knuckleheads")
+puts knuckleheads.title
+
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+
+knuckleheads.play
