4ebf55cfa77b261278e2993ed2343406394cf64e
Navleen Kaur <navleenkaur@Navleens-MacBook-Pro.local>
2022-08-10 11:29:18 -0400

    Cleaned up code

--- studio_game.rb
+++ studio_game.rb
@@ -58,35 +58,7 @@ player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
 
-players = [player1, player2, player3]
-
-puts "There are #{players.size} players in the game:"
-
-players.each do |player|
-    puts player
-end
-  
-players.each do |player|
-    puts player.health
-end
-
-players.each do |player|
-    player.blam
-    player.w00t
-    player.w00t
-    puts player
-end
-
-players.pop
-player4 = Player.new("Shemp", 90)
-players.push(player4)
-puts players
-
-
-puts "____________________"
-
 knuckleheads = Game.new("Knuckleheads")
-puts knuckleheads.title
 
 knuckleheads.add_player(player1)
 knuckleheads.add_player(player2)
