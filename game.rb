539c763c0db09e9ecd3996653e0e988758c09527
Navleen Kaur <navleenkaur@Navleens-MacBook-Pro.local>
2022-08-11 10:34:08 -0400

    Set up die class and tests

--- game.rb
+++ game.rb
@@ -1,4 +1,5 @@
 require_relative "player"
+require_relative "die"
 
 class Game
 
@@ -20,9 +21,28 @@ class Game
         end
 
         @players.each do |player|
-            player.blam
-            player.w00t
-            player.w00t
+            die = Die.new
+            number_rolled = die.roll
+
+        # **if conditional for game play**
+            # if number_rolled < 3
+            #     player.blam
+            # elsif number_rolled > 4
+            #     player.w00t
+            # else
+            #     puts "#{player.name} was skipped."
+            # end
+
+        # **when clause for game play**
+            case die.roll
+            when 1..2
+              player.blam
+            when 3..4
+              puts "#{player.name} was skipped."
+            else
+              player.w00t
+            end
+
             puts player
         end
     end
